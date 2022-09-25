877892cb29ef03e821dd0298075b56c3950b5dc1
Gulshan Kumar <arun123gulshan@gmail.com>
2022-09-25 13:03:07 +0530

    current loaction

--- app/src/main/java/com/gulshan/myapplication/ui/map/MapsFragment.java
+++ app/src/main/java/com/gulshan/myapplication/ui/map/MapsFragment.java
@@ -2,23 +2,48 @@ package com.gulshan.myapplication.ui.map;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationRequest;
+import android.Manifest;
+
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.gulshan.myapplication.R;
 
 public class MapsFragment extends Fragment {
 
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 100;
+    private static final int RESULT_OK = 100;
+    private boolean locationPermissionGranted;
+    GoogleMap map;
+    LocationRequest request;
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
         /**
@@ -32,11 +57,24 @@ public class MapsFragment extends Fragment {
          */
         @Override
         public void onMapReady(GoogleMap googleMap) {
+            String provider = Settings.Secure.getString(getActivity().getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+
+            map = googleMap;
+
+            googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                 return;
+            }
+            googleMap.setMyLocationEnabled(true);
+            checkGps();
             LatLng sydney = new LatLng(-34, 151);
             googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
             googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+           getLocationPermission();
         }
     };
+    private Object lastKnownLocation;
+    private Thread locationRequest;
 
     @Nullable
     @Override
@@ -44,6 +82,7 @@ public class MapsFragment extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_maps, container, false);
+
     }
 
     @Override
@@ -55,5 +94,105 @@ public class MapsFragment extends Fragment {
             mapFragment.getMapAsync(callback);
         }
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        locationPermissionGranted = false;
+        if (requestCode
+                == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {// If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationPermissionGranted = true;
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        updateLocationUI();
+    }
 
+
+        private void updateLocationUI() {
+            if (map == null) {
+                return;
+            }
+            try {
+                if (locationPermissionGranted) {
+                    map.setMyLocationEnabled(true);
+                    map.getUiSettings().setMyLocationButtonEnabled(true);
+                } else {
+                    map.setMyLocationEnabled(false);
+                    map.getUiSettings().setMyLocationButtonEnabled(false);
+                    lastKnownLocation = null;
+                    getLocationPermission();
+                }
+            } catch (SecurityException e)  {
+                Log.e("Exception: %s", e.getMessage());
+            }
+    }
+
+    private void getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+            checkGps();
+        } else {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+
+
+    private void checkGps() {
+        request = request.create();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setInterval(5000);
+        request.setFastestInterval(3000);
+        LocationSettingsRequest.Builder builser = new LocationSettingsRequest.Builder().addLocationRequest(request).setAlwaysShow(true);
+        Task<LocationSettingsResponse> locationSettingsRequestTask = LocationServices.getSettingsClient(getActivity()).checkLocationSettings(builser.build());
+
+        locationSettingsRequestTask.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+                try {
+                    LocationSettingsResponse request = task.getResult(ApiException.class);
+                    Toast.makeText(getActivity(), "Gps Is Enabled", Toast.LENGTH_SHORT).show();
+                } catch (ApiException e) {
+                    if (e.getStatusCode()== LocationSettingsStatusCodes.RESOLUTION_REQUIRED)
+                    {
+                        ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                        try {
+                            resolvableApiException.startResolutionForResult(getActivity(),101);
+                        } catch (IntentSender.SendIntentException sendIntentException) {
+                            sendIntentException.printStackTrace();
+                        }
+                    }
+                    if(e.getStatusCode()==LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE)
+                    {
+                        Toast.makeText(getActivity(), "Setting prohibited  ", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==101) {
+
+            if(resultCode==getActivity().RESULT_OK) {
+                Toast.makeText(getActivity(), "GPS on", Toast.LENGTH_SHORT).show();
+            }
+            else if(resultCode==getActivity().RESULT_CANCELED) {
+                Toast.makeText(getActivity(), "GPS unable to access", Toast.LENGTH_SHORT).show();
+
+            }
+
+        }
+    }
 }
